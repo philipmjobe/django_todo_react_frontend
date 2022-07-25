cca9dabe20e7f4c86cbeb3418e25a8959a00fc65
Philip <philipmjobe@gmail.com>
2022-07-25 14:03:34 -0400

    fixes some css issues and gets page to load with with todo list

--- src/App.js
+++ src/App.js
@@ -63,11 +63,13 @@ class App extends Component {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter((item) => item.completed == viewCompleted);
     return newItems.map((item) => (
-      <li key={item.id} className="list-group-item d-flex justify-content-between aligh-items-center">
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
         <span
           className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}
-          title={newItems.description}
-        ></span>
+          title={item.description}
+        >
+          {item.title}
+        </span>
         <span>
           <button className="btn btn-secondary mr-2">Edit</button>
           <button className="btn btn-danger">Delete</button>
@@ -86,7 +88,7 @@ class App extends Component {
                 <button className="btn btn-primary">Add task</button>
               </div>
               {this.renderTabList()}
-              <ul className="list-group list-group-flash border-top-0">{this.render()}</ul>
+              <ul className="list-group list-group-flush border-top-0">{this.renderItems()}</ul>
             </div>
           </div>
         </div>
