486de96f768873b411f9980c182b7d64edcdc6e2
Philip <philipmjobe@gmail.com>
2022-07-25 15:35:30 -0400

    installs axios using npm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Modal from "./components/Modal";
 
 const todoItems = [
   {
@@ -33,8 +34,37 @@ class App extends Component {
     this.state = {
       viewCompleted: false,
       todoList: todoItems,
+      modal: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false,
+      },
     };
   }
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = (item) => {
+    this.toggle();
+    alert("save" + JSON.stringify(item));
+  };
+
+  handleDelete = (item) => {
+    alert("delete" + JSON.stringify(item));
+  };
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  editItem = (item) => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
   displayCompleted = (status) => {
     if (status) {
       return this.setState({ viewCompleted: true });
@@ -61,7 +91,7 @@ class App extends Component {
   };
   renderItems = () => {
     const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter((item) => item.completed == viewCompleted);
+    const newItems = this.state.todoList.filter((item) => item.completed === viewCompleted);
     return newItems.map((item) => (
       <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
         <span
@@ -71,8 +101,12 @@ class App extends Component {
           {item.title}
         </span>
         <span>
-          <button className="btn btn-secondary mr-2">Edit</button>
-          <button className="btn btn-danger">Delete</button>
+          <button className="btn btn-secondary mr-2" onClick={() => this.editItem(item)}>
+            Edit
+          </button>
+          <button className="btn btn-danger" onClick={() => this.handleDelete(item)}>
+            Delete
+          </button>
         </span>
       </li>
     ));
@@ -85,13 +119,18 @@ class App extends Component {
           <div className="col-md-6 col-sm10 mx-auto p-0">
             <div className="card p-3">
               <div className="mb-4">
-                <button className="btn btn-primary">Add task</button>
+                <button className="btn btn-primary" onClick={this.createItem}>
+                  Add task
+                </button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush border-top-0">{this.renderItems()}</ul>
             </div>
           </div>
         </div>
+        {this.state.modal ? (
+          <Modal activeItem={this.state.activeItem} toggle={this.toggle} onSave={this.handleSubmit} />
+        ) : null}
       </main>
     );
   }
